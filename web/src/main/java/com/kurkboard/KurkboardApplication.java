9645e613dfd1b79f5703d4e633515f36d3aab4a6
PublicVoidToString <kml.pieper@gmail.com>
2026-09-20 00:00:45 +0200

    Connecting Docker Database

---
+++ web/src/main/java/com/kurkboard/KurkboardApplication.java
@@ -0,0 +1,12 @@
+package com.kurkboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurkboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurkboardApplication.class, args);
+    }
+}
